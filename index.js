8b911540db7840e16fd50f7deedf5e0181a4192a
fureya48 <fathur.rohman2535@gmail.com>
2022-08-11 03:51:47 +0700

    add project chapter 5

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+const app = express();
+
+const user = {
+  username: "admin",
+  password: "admin123",
+};
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  const { password } = req.body;
+  if (username == user.username && password == user.password) {
+    res.render("home");
+  } else {
+    res.send("Email/Password anda salah!");
+  }
+});
+
+app.get("/logout", (req, res) =>{
+  res.redirect("/login")
+})
+
+app.use('/', (req, res) =>{
+  res.status(404).send("Not Found")
+})
+
+app.listen(3001, () => {
+  console.log("Server connected at 3001!");
+});
