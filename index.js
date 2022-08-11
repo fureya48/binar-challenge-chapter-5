53e037c8069edbc613f43f9582bb96b5b4777514
fureya48 <fathur.rohman2535@gmail.com>
2022-08-12 00:19:02 +0700

    handle middleware for block home page

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import express from "express";
 const app = express();
 
-const user = {
-  username: "admin",
-  password: "admin123",
-};
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+
+const user = {
+  username: "admin",
+  password: "admin123",
+};
 
 app.get("/", (req, res) => {
   res.redirect("/login");
@@ -20,27 +20,27 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.post("/login", (req, res) => {
-  const { username } = req.body;
-  const { password } = req.body;
-  if (username == user.username && password == user.password) {
-    res.render("home");
-  } else {
-    res.send("Email/Password anda salah!");
-  }
+app.post("/home", (req, res) => {
+    const { username } = req.body;
+    const { password } = req.body;
+    if (username == user.username && password == user.password) {
+      res.render("home");
+    } else {
+      res.send("Email/Password anda salah!");
+    }
+  });
+
+app.get("/game", (req, res) => {
+  res.render("game");
 });
 
-app.get("/login/game", (req, res) =>{
-  res.render("game");
-})
+app.get("/logout", (req, res) => {
+  res.redirect("/login");
+});
 
-app.get("/logout", (req, res) =>{
-  res.redirect("/login")
-})
-
-app.use('/', (req, res) =>{
-  res.status(404).send("Not Found")
-})
+app.use("/", (req, res) => {
+  res.status(404).send("<h1>Not Found</h1>");
+});
 
 app.listen(3001, () => {
   console.log("Server connected at 3001!");
