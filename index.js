caafe0601693cf796672771d962143ae4901b8dd
fureya48 <fathur.rohman2535@gmail.com>
2022-08-14 01:23:08 +0700

    finishing code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import users from "./data/users.json" assert {type: "json"}
+import users from "./data/users.json" assert { type: "json" };
 const app = express();
 
 app.use(express.json());
@@ -8,46 +8,66 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+let findUser;
+let getUsername;
+let message;
+
 app.get("/", (req, res) => {
+  res.status(302);
   res.redirect("/login");
 });
 
 app.get("/login", (req, res) => {
-  res.render("login");
+  res.status(200);
+  res.render("login", {
+    message
+  });
 });
 
-app.post("/home", (req, res) => {
+app.post("/login", (req, res) => {
   const { username } = req.body;
   const { password } = req.body;
-  const user = users.find((user) => user.username == username);
-  if (username == user.username && password == user.password) {
-    res.render("home", {
-      user: user
-    });
+  findUser = users.find((user) => {
+    return username == user.username && password == user.password;
+  });
+  if (findUser) {
+    res.status(302);
+    res.redirect("/home");
   } else {
-    res.send("Email/Password anda salah!");
+    res.render("login", {
+      message: "Username or password is wrong!",
+    });
   }
+  return findUser;
 });
 
-app.use((req,res, next)=>{
-  console.log("middleware............")
-  return next()
-})
-
-app.get("/game/:user", (req, res) => {
-  res.render("game");
+app.use((req, res, next) => {
+  getUsername = findUser.username;;
+  next();
+  return getUsername;
 });
 
-app.get("/home/:user", (req, res) => {
-  res.render("home");
+app.get("/home", (req, res) => {
+  res.status(200);
+  res.render("home", {
+    username: getUsername,
+  });
+});
+
+app.get("/game/:username", (req, res) => {
+  res.status(200);
+  res.render("game", {
+    username: getUsername,
+  });
 });
 
 app.get("/logout", (req, res) => {
+  res.status(302);
   res.redirect("/login");
 });
 
 app.use("/", (req, res) => {
-  res.status(404).send("<h1>Not Found</h1>");
+  res.status(404).send("<h1>404 Not Found</h1>");
 });
 
 app.listen(3001, () => {
