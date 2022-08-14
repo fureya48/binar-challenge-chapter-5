6f3db6d9ae88ee065d7e2d73398c2852797e5ce3
fureya48 <fathur.rohman2535@gmail.com>
2022-08-14 18:31:50 +0700

    change PORT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import hash from "object-hash";
 import users from "./data/users.json" assert { type: "json" };
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -77,6 +78,6 @@ app.use("/", (req, res) => {
   res.status(404).send("<h1>404 Not Found</h1>");
 });
 
-app.listen(3001, () => {
-  console.log("Server connected at 3001!");
+app.listen(3000, () => {
+  console.log("Server connected at 3000!");
 });
