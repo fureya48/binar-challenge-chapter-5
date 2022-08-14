c6db673d51ef6d6e0fd263f8c9325d3b4aefacab
fureya48 <fathur.rohman2535@gmail.com>
2022-08-14 18:32:28 +0700

    change variable PORT

--- index.js
+++ index.js
@@ -78,6 +78,6 @@ app.use("/", (req, res) => {
   res.status(404).send("<h1>404 Not Found</h1>");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server connected at 3000!");
 });
